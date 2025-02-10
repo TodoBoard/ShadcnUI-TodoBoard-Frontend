548391ace304e5038224a8f74266861dc3f7fbd8
KonBuku <wagnerkonrad09@gmail.com>
2025-02-10 21:31:54 +0100

    [add] dock navbar

--- src/app/board/projects/my-projects/[project-id]/page.tsx
+++ src/app/board/projects/my-projects/[project-id]/page.tsx
@@ -1,9 +1,22 @@
 import { NoTasks } from "@/app/modules/board/ui/components/no-tasks";
+import { Plus } from "lucide-react";
 
 export default function MyProjectsTravelPage() {
   return (
     <div>
       <h1 className="font-bold text-2xl">Travel</h1>
+
+      <div className="flex items-center mt-5">
+        <button className="flex items-center relative group">
+          <span className="flex items-center justify-center w-8 h-8 rounded-full bg-transparent group-hover:bg-purple-500 transition duration-300">
+            <Plus className="text-primary group-hover:text-white transition duration-300" />
+          </span>
+          <span className="ml-2 text-gray-500 cursor-pointer group-hover:text-purple-500 transition duration-300">
+            Add Task
+          </span>
+        </button>
+      </div>
+
       <NoTasks />
     </div>
   );
--- src/app/modules/home/ui/header/navbar.tsx
+++ src/app/modules/home/ui/header/navbar.tsx
@@ -1,3 +1,117 @@
+"use client";
+
+import { CalendarCheck2, Github, Linkedin, X, Mail } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { buttonVariants } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
+import { Dock, DockIcon } from "@/components/ui/dock";
+
+export type IconProps = React.HTMLAttributes<SVGElement>;
+
+const DATA = {
+  navbar: [
+    {
+      href: "#",
+      icon: CalendarCheck2,
+      label: "TodoApp",
+      className: "text-primary",
+    },
+  ],
+  contact: {
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "#",
+        icon: Github,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "#",
+        icon: Linkedin,
+      },
+      X: {
+        name: "X",
+        url: "#",
+        icon: X,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Mail,
+      },
+    },
+  },
+};
+
 export function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <TooltipProvider>
+      <Dock direction="middle">
+        {DATA.navbar.map((item) => (
+          <DockIcon key={item.label}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href={item.href}
+                  aria-label={item.label}
+                  className={cn(
+                    buttonVariants({ variant: "ghost", size: "icon" }),
+                    "size-12 rounded-full"
+                  )}
+                >
+                  <item.icon className={item.className} />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{item.label}</p>
+              </TooltipContent>
+            </Tooltip>
+          </DockIcon>
+        ))}
+        <Separator orientation="vertical" className="h-full" />
+        {Object.entries(DATA.contact.social).map(([name, social]) => (
+          <DockIcon key={name}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href={social.url}
+                  aria-label={social.name}
+                  className={cn(
+                    buttonVariants({ variant: "ghost", size: "icon" }),
+                    "size-12 rounded-full"
+                  )}
+                >
+                  <social.icon className="size-4" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{name}</p>
+              </TooltipContent>
+            </Tooltip>
+          </DockIcon>
+        ))}
+        <Separator orientation="vertical" className="h-full py-2" />
+        <DockIcon>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <ModeToggle />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Theme</p>
+            </TooltipContent>
+          </Tooltip>
+        </DockIcon>
+      </Dock>
+    </TooltipProvider>
+  );
 }
