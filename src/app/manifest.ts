b1fd7b37c29eebd71911a0d76258c9454e5d48d4
KonBuku <wagnerkonrad09@gmail.com>
2025-02-22 16:42:44 +0100

    [add] pwa-support

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "TodoBoard",
+    short_name: "TodoBoard",
+    description: "Open source Todo App.",
+    start_url: "/auth/login",
+    display: "standalone",
+    background_color: "#09090B",
+    theme_color: "#09090B",
+    icons: [
+      {
+        src: '/pwa/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/pwa/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
